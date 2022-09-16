4ff9339583b553dd5b24d130ef978c7274df7103
ScottDrysdale86 <sdrysdale86@pm.me>
2022-09-16 21:22:17 +0100

    finished the project pages. changed to more or less links

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,8 +1,140 @@
 import styled from "styled-components";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Carousel, CarouselItem } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Projects = () => {
+	const [extraContent, setExtraContent] = useState(false);
+
+	const extraContentPython = (
+		<>
+			<SubHeading>MVP</SubHeading>
+			<ListedTasks>
+				<Task>
+					The app should allow the user to create, update and delete
+					an employee. CRUD
+				</Task>
+				<Task>
+					Each employee will have a name, a set organisation level and
+					log in credentials.
+				</Task>
+				<Task>
+					Each log in credential will have a pin and a passcode.
+				</Task>
+				<Task>There will be 2 level types (staff and manager)</Task>
+				<Task>
+					The user should be able to filter by level to display all
+					employees within that level
+				</Task>
+			</ListedTasks>
+			<SubHeading>Extension</SubHeading>
+			<ListedTasks>
+				<Task>
+					Using the credentials, the employee should be able to clock
+					in and clock out.
+				</Task>
+				<Task>
+					Employee can only clock in if credentials are correct.
+				</Task>
+				<Task>
+					Employee should only be able to clock out if they have
+					already clocked in
+				</Task>
+				<Task>
+					Create a filter which will show a list of employees’ clock
+					ins for a week.
+				</Task>
+			</ListedTasks>
+			<SubHeading>If I Had More Time:</SubHeading>
+			<Description>
+				Staff member clock in validation <br />
+				Show individual clock’s by staff member <br />
+				Total each staff members hours
+				<br />
+				Cross reference contracted hours to clocked hours for a given
+				week.
+			</Description>
+			<SubHeading>Reflection</SubHeading>
+			<Description>
+				This was a good and challenging first project after learning the
+				tech stack for only 4 weeks. I learned that working with
+				DateTime can be tricky and to watch out for columns with the
+				same name when designing/joining database tables. Overall I
+				enjoyed this project and gained further understanding of using
+				Flask and RestFul routes
+			</Description>
+		</>
+	);
+
+	const extraContentFootball = (
+		<>
+			<SubHeading>MVP</SubHeading>
+			<ListedTasks>
+				<Task>
+					Filter various football leagues from around the world
+				</Task>
+				<Task>Filter by season to show results.</Task>
+				<Task>
+					Can also select a team in the league displayed to get more
+					information.
+				</Task>
+			</ListedTasks>
+			<SubHeading>If I Had More Time</SubHeading>
+			<Description>
+				Spend more time working with Google Charts as this was added in
+				last minute. Look at optimising the initial loading as it
+				appears to fetch twice on start up
+			</Description>
+			<SubHeading>Reflection</SubHeading>
+			<Description>
+				I had fun making and designing this and overall I am happy with
+				what I achieved as it was only approx 2 days worth of work.
+			</Description>
+		</>
+	);
+
+	const extraContentJs = (
+		<>
+			<SubHeading>MVP</SubHeading>
+			<ListedTasks>
+				<Task>
+					Model a deck of cards presented to the user in a random
+					order.
+				</Task>
+				<Task>
+					Implement rules for card stacking that prevent illegal moves
+				</Task>
+				<Task>
+					Allow the user to flip cards from the deck which they can
+					then interact with
+				</Task>
+				<Task>Allow the user to start/end the game</Task>
+			</ListedTasks>
+			<SubHeading>Extension</SubHeading>
+			<ListedTasks>
+				<Task>
+					Total the users score and take in a name for a leaderboard
+				</Task>
+				<Task>Hint functionality</Task>
+				<Task>Easier version with some relaxed rules</Task>
+				<Task>The game recognising when there are no legal moves</Task>
+			</ListedTasks>
+			<SubHeading>If I had more time</SubHeading>
+			<Description>
+				Add a hint function to help the user. Ad an undo button to step
+				back a move. Implement drag and drop instead of click to select.
+			</Description>
+			<SubHeading>Reflection</SubHeading>
+			<Description>
+				Overall was a great team project where communication was key. We
+				had to ensure that we were all aware of what each other was
+				workling on. This was done by having daily stand-ups as well as
+				constant messaging.
+			</Description>
+		</>
+	);
+	const linkName = extraContent ? "Less Info << " : "More Info >> ";
+
 	useEffect(() => {
 		const obj = document.querySelector("#videos");
 		obj.defaultPlaybackRate = 2;
@@ -18,6 +150,7 @@ const Projects = () => {
 						<Title>
 							Employee Management System <br /> Solo - 1 week
 						</Title>
+
 						<Pic src={require("../images/EMS.png")} alt="EMS"></Pic>
 						<SubHeading>Task</SubHeading>
 						<Description>
@@ -31,66 +164,17 @@ const Projects = () => {
 							Psycopg2 was used as the database adaptor. <br />
 							Front-End: HTML, CSS
 						</Description>
-						<SubHeading>MVP</SubHeading>
-						<ListedTasks>
-							<Task>
-								The app should allow the user to create, update
-								and delete an employee. CRUD
-							</Task>
-							<Task>
-								Each employee will have a name, a set
-								organisation level and log in credentials.
-							</Task>
-							<Task>
-								Each log in credential will have a pin and a
-								passcode.
-							</Task>
-							<Task>
-								There will be 2 level types (staff and manager)
-							</Task>
-							<Task>
-								The user should be able to filter by level to
-								display all employees within that level
-							</Task>
-						</ListedTasks>
-						<SubHeading>Extension</SubHeading>
-						<ListedTasks>
-							<Task>
-								Using the credentials, the employee should be
-								able to clock in and clock out.
-							</Task>
-							<Task>
-								Employee can only clock in if credentials are
-								correct.
-							</Task>
-							<Task>
-								Employee should only be able to clock out if
-								they have already clocked in
-							</Task>
-							<Task>
-								Create a filter which will show a list of
-								employees’ clock ins for a week.
-							</Task>
-						</ListedTasks>
-						<SubHeading>If I Had More Time:</SubHeading>
-						<Description>
-							Staff member clock in validation <br />
-							Show individual clock’s by staff member <br />
-							Total each staff members hours
-							<br />
-							Cross reference contracted hours to clocked hours
-							for a given week.
-						</Description>
-						<SubHeading>Reflection</SubHeading>
-						<Description>
-							This was a good and challenging first project after
-							learning the tech stack for only 4 weeks. I learned
-							that working with DateTime can be tricky and to
-							watch out for columns with the same name when
-							designing/joining database tables. Overall I enjoyed
-							this project and gained further understanding of
-							using Flask and RestFul routes
-						</Description>
+						<ExtraBox>
+							<ExtraLink
+								className="read-more-link"
+								onClick={() => {
+									setExtraContent(!extraContent);
+								}}
+							>
+								<ExtraTitle>{linkName}</ExtraTitle>
+							</ExtraLink>
+							{extraContent && extraContentPython}
+						</ExtraBox>
 						<GitLink
 							href="https://github.com/ScottDrysdale86/employee_management_system_project"
 							target="_blank"
@@ -115,24 +199,26 @@ const Projects = () => {
 							type="video/mp4"
 						></Vids>
 						<SubHeading>Task</SubHeading>
-						<Description>asdfghjk</Description>
-						<SubHeading>Tech</SubHeading>
-						<Description>sdfghjkl</Description>
-						<SubHeading>MVP</SubHeading>
-						<Description>sdfghjkl</Description>
-						<SubHeading>Extension</SubHeading>
-						<Description>sdfghjkl</Description>
-						<SubHeading>If I had more time</SubHeading>
-						<Description>sdfghjkl</Description>
-						<SubHeading>Reflection</SubHeading>
-						<Description>sdfghjkl</Description>
 						<Description>
-							React App utilising an API to access and display
-							data. User can filter various football leagues from
-							around the world as well as filter by season to show
-							results. Can also select a team in the league
-							displayed to get more information.
+							Create an application that makes a request to an API
+							and displays the data.
 						</Description>
+						<SubHeading>Tech</SubHeading>
+						<Description>
+							React.js, Styled Components, API's, CSS
+						</Description>
+						<ExtraBox>
+							<ExtraLink
+								className="read-more-link"
+								onClick={() => {
+									setExtraContent(!extraContent);
+								}}
+							>
+								<ExtraTitle>{linkName}</ExtraTitle>
+							</ExtraLink>
+							{extraContent && extraContentFootball}
+						</ExtraBox>
+
 						<GitLink
 							href="https://github.com/ScottDrysdale86/Week7-reactApp-API-football"
 							target="_blank"
@@ -156,21 +242,26 @@ const Projects = () => {
 							type="video/mp4"
 						></Vids>
 						<SubHeading>Task</SubHeading>
-						<Description>asdfghjk</Description>
-						<SubHeading>Tech</SubHeading>
-						<Description>sdfghjkl</Description>
-						<SubHeading>MVP</SubHeading>
-						<Description>sdfghjkl</Description>
-						<SubHeading>Extension</SubHeading>
-						<Description>sdfghjkl</Description>
-						<SubHeading>If I had more time</SubHeading>
-						<Description>sdfghjkl</Description>
-						<SubHeading>Reflection</SubHeading>
-						<Description>sdfghjkl</Description>
 						<Description>
-							React app which allows the user to play a
-							solitaire-based card game Devil’s Grip.
+							The react app allows the user to play the solitaire
+							based card game called Devil's Grip.
 						</Description>
+						<SubHeading>Tech</SubHeading>
+						<Description>
+							Back-end: MongoDB and Express. Front-end: React.js,
+							HTML, CSS, Styled Components.
+						</Description>
+						<ExtraBox>
+							<ExtraLink
+								className="read-more-link"
+								onClick={() => {
+									setExtraContent(!extraContent);
+								}}
+							>
+								<ExtraTitle>{linkName}</ExtraTitle>
+							</ExtraLink>
+							{extraContent && extraContentJs}
+						</ExtraBox>
 						<GitLink>Link to Source</GitLink>
 					</ProjectBox>
 				</CarouselItem>
@@ -206,6 +297,11 @@ const ProjectBox = styled.section`
 	aspect-ratio: 3/1;
 `;
 
+const ExtraBox = styled.section`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
 const Title = styled.h2`
 	text-align: center;
 	text-decoration: underline;
@@ -269,6 +365,17 @@ const Task = styled.li`
 	text-align: center;
 	color: #c5c1c0;
 	font-size: 2vh;
+`;
+const ExtraTitle = styled.h2`
+	text-decoration: underline;
+	margin: 0;
+	color: #ff6c02;
+	margin-bottom: 3%;
+	font-size: 3vh;
+`;
+const ExtraLink = styled.a`
+	text-decoration: none;
+	list-style: none;
 `;
 
 export default Projects;
